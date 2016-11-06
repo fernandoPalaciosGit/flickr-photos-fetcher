79c1c51fa5d15ae112323d0a22a0a08ad6a816a3
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-11-06 20:06:10 +0100

    test fetcherFlickr to compose url and title template view client

--- front-layer/test/component/FlickrFetcher.js
+++ front-layer/test/component/FlickrFetcher.js
@@ -2,12 +2,39 @@
 
 var expect = require('chai').expect;
 
-describe('FlickrFetcher', function() {
-    it('should exist', function(next) {
-        var FlickrFetcher = require('./../../main/component/FlickrFetcher'),
-            flicker = new FlickrFetcher({search: 'scotish terriers'});
+describe('FlickrFetcher', function () {
+    var FlickrFetcher = require('./../../main/component/FlickrFetcher'), flicker, testFlickrData;
 
+    before(function () {
+        flicker = new FlickrFetcher({search: 'scotish terriers'});
+        testFlickrData = {
+            id: '24770505034',
+            owner: '97248275@N03',
+            secret: '31a9986429',
+            server: '1577',
+            farm: 2,
+            title: 'Dog goes to desperate measure to avoid walking on a leash',
+            ispublic: 1,
+            isfriend: 0,
+            isfamily: 0
+        };
+    });
+
+    it('should end a request to the Flickr API, and retrieve a bunch of photograph data;', function (next) {
         expect(flicker.fetchPhotos).to.be.a('function');
+        next();
+    });
+
+    it('should take a photo object and return an object with just title and URI', function (next) {
+        var expected = {
+                title: 'Dog goes to desperate measure to avoid walking on a leash',
+                url: 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg'
+            },
+            actualUrl = flicker.getImageURL(testFlickrData),
+            actualObject = flicker.getDataTemplate([testFlickrData]);
+
+        expect(actualUrl).to.equal(expected.url);
+        expect(actualObject[0]).to.eql(expected);
         next();
     });
 });
---
+++ front-layer/test/component/UI.FlickrPhoto.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('UI FlickrPhoto', function() {
+    it('should transform the data into a single array of objects, each object containing just the data we need', function(next) {
+        expect(true).to.be.true;
+        next();
+    });
+
+    it('should Convert the array of objects into an HTML list', function(next) {
+        next();
+    });
+
+    it('should Add the HTML to the page.', function(next) {
+        next();
+    });
+
+});
